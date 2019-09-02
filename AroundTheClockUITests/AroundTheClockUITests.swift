54bcc79a29d6b1703e1807ab9da44dbec001cfe7
Ryan Angelo <rangelo89@gmail.com>
2019-09-02 11:19:23 -0400

    Updated project settings for Swift 5.

--- AroundTheClockUITests/AroundTheClockUITests.swift
+++ AroundTheClockUITests/AroundTheClockUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Around_The_Clock
 
 class AroundTheClockUITests: XCTestCase {
     
@@ -16,6 +17,8 @@ class AroundTheClockUITests: XCTestCase {
         continueAfterFailure = false
         let application = XCUIApplication()
         application.launchEnvironment = ["UITESTS":"1"]
+        
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         application.launch()
 
